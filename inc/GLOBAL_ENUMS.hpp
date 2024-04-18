07dcbabad2da39236112441cc8c536d91ae29b37
Abdo Eid <abdoeid.eg@ieee.org>
2024-04-18 03:41:47 +0200

    Cleaned main.cpp

--- inc/GLOBAL_ENUMS.hpp
+++ inc/GLOBAL_ENUMS.hpp
@@ -4,78 +4,93 @@
 #include <vector>
 
 /* Node */
-struct node_position{
-    float x, y;
+struct node_position {
+  float x, y;
 };
 
-enum node_state_index_enum{
-    rest = 1,
-    ERP = 2,
-    RRP = 3
-};
+enum node_state_index_enum { rest = 1, ERP = 2, RRP = 3 };
 
 // The terminal of the path connected to the node
-enum path_terminal_enum{
-    __entry = 1, // Then, this is named like this for consistency 
-    __exit = 2   // Firstly, named like this because of the conflict with the exit keyword. The exit function is a standard function in C++ that is used to terminate a program. It is defined in the <cstdlib> header file.
+enum path_terminal_enum {
+  __entry = 1,  // Then, this is named like this for consistency
+  __exit = 2  // Firstly, named like this because of the conflict with the exit
+              // keyword. The exit function is a standard function in C++ that
+              // is used to terminate a program. It is defined in the <cstdlib>
+              // header file.
 };
 
 // a struct that has path_idx, terminal pair for each path connected to the node
-struct path_terminal_pair{
-    int path_idx;
-    path_terminal_enum terminal;
+struct path_terminal_pair {
+  int path_idx;
+  path_terminal_enum terminal;
 };
 
-// Is the node an Atrioventricular node related or not as they have different resopnse pattern
-enum AVness_enum
-{
-    AV = 1,
-    non_AV = 2
+// Is the node an Atrioventricular node related or not as they have different
+// resopnse pattern
+enum AVness_enum { AV = 1, non_AV = 2 };
+
+struct node_parameters {
+  std::string node_name;
+  node_state_index_enum node_state_index =
+      rest;  // 1 for rest, 2 for ERP, 3 for RRP
+  // The total refractory period is comprised of the (1) Rset, (2) Conduction,
+  // (3) ERP, (4) RRP, and (5) Trest periods. Starting or Ending with a Rest
+  // period.
+  int TERP_current;
+  int TERP_default;  // The ERP (Effective Refractory Period) refers to the
+                     // period during which cardiac cells are unable to respond
+                     // to a new electrical stimulus, regardless of its
+                     // strength.
+  int TRRP_current;
+  int TRRP_default;  // The RRP (Relative Refractory Period) follows the ERP and
+                     // represents a phase during which cardiac cells have
+                     // partially recovered their excitability. While the cells
+                     // are still in a refractory state, they are more
+                     // responsive to strong stimuli compared to during the
+                     // ERP.RRP is shorter than that of the ERP.
+  int Trest_current;
+  int Trest_default;  // The Trest (Rest) period is the period during which the
+                      // cell is not excitable and is not responsive to any
+                      // stimulus.
+  bool activation;    // 1 for activated, 0 for not activated
+  int Terp_min;       // The minimum effective refractory period
+  int Terp_max;       // The maximum effective refractory period
+  int index_of_path_activate_the_node;  // The index of the path that activates
+                                        // the node, used to exclude the path
+                                        // from the list of paths that the node
+                                        // affects
+  AVness_enum AVness;  // Is the node an Atrioventricular node related or not as
+                       // they have
+  std::vector<path_terminal_pair>
+      connected_paths;     // The paths that are connected to the node
+  node_position node_pos;  // The position of the node in the grid
 };
-
-struct node_parameters{
-        std::string node_name;
-        node_state_index_enum node_state_index = rest; // 1 for rest, 2 for ERP, 3 for RRP
-        // The total refractory period is comprised of the (1) Rset, (2) Conduction, (3) ERP, (4) RRP, and (5) Trest periods. Starting or Ending with a Rest period. 
-        int TERP_current; 
-        int TERP_default;   // The ERP (Effective Refractory Period) refers to the period during which cardiac cells are unable to respond to a new electrical stimulus, regardless of its strength.
-        int TRRP_current;   
-        int TRRP_default;   // The RRP (Relative Refractory Period) follows the ERP and represents a phase during which cardiac cells have partially recovered their excitability. While the cells are still in a refractory state, they are more responsive to strong stimuli compared to during the ERP.RRP is shorter than that of the ERP.
-        int Trest_current; 
-        int Trest_default;  // The Trest (Rest) period is the period during which the cell is not excitable and is not responsive to any stimulus.
-        bool activation;    // 1 for activated, 0 for not activated
-        int Terp_min;       // The minimum effective refractory period
-        int Terp_max;       // The maximum effective refractory period
-        int index_of_path_activate_the_node;    // The index of the path that activates the node, used to exclude the path from the list of paths that the node affects
-        AVness_enum AVness; // Is the node an Atrioventricular node related or not as they have 
-        std::vector<path_terminal_pair> connected_paths; // The paths that are connected to the node
-        node_position node_pos; // The position of the node in the grid
-    };
-
 
 /* Path */
-enum path_state_index_enum{
-    Idle = 1,
-    Antegrade_conduction = 2,
-    Retrograde = 3,
-    Conflict = 4,
-    Double = 5
+enum path_state_index_enum {
+  Idle = 1,
+  Antegrade_conduction = 2,
+  Retrograde = 3,
+  Conflict = 4,
+  Double = 5
 };
 
-struct path_parameters{
-    std::string path_name;                      // The name of the path
-    path_state_index_enum path_state_index;     // (1) Idle, (2) Antegrade conduction, (3) Retrograde, (4) Conflict, and (5) Double
-    int entry_node_index;                       // The index of the node that the path starts from
-    int exit_node_index;                        // The index of the node that the path ends at
-    int amplitude_factor;                       // The amplitude factor of the path, i.e. the Gain
-    float forward_speed;                        // The speed of the wavefront in the forward direction
-    float backward_speed;                       // The speed of the wavefront in the backward direction
-    float forward_timer_current;                // The current timer for the forward direction
-    float forward_timer_default;                // The default timer for the forward direction
-    float backward_timer_current;               // The current timer for the backward direction
-    float backward_timer_default;               // The default timer for the backward direction
-    float path_length;                          // The length of the path
-    float path_slope;                           // The slope of the path
+struct path_parameters {
+  std::string path_name;  // The name of the path
+  path_state_index_enum
+      path_state_index;  // (1) Idle, (2) Antegrade conduction, (3) Retrograde,
+                         // (4) Conflict, and (5) Double
+  int entry_node_index;  // The index of the node that the path starts from
+  int exit_node_index;   // The index of the node that the path ends at
+  int amplitude_factor;  // The amplitude factor of the path, i.e. the Gain
+  float forward_speed;   // The speed of the wavefront in the forward direction
+  float backward_speed;  // The speed of the wavefront in the backward direction
+  float forward_timer_current;   // The current timer for the forward direction
+  float forward_timer_default;   // The default timer for the forward direction
+  float backward_timer_current;  // The current timer for the backward direction
+  float backward_timer_default;  // The default timer for the backward direction
+  float path_length;             // The length of the path
+  float path_slope;              // The slope of the path
 };
 
-#endif // _GLOBAL_ENUMS_HPP
+#endif  // _GLOBAL_ENUMS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,107 @@
 #include <QGuiApplication>
+#include <QObject>
 #include <QQmlApplicationEngine>
 #include <QTimer>
-#include <QObject>
-#include <iostream>
 #include <cmath>
+#include <iostream>
 using std::vector, std::string, std::pair;
-#include "../inc/heart_model.hpp"
 #include "../inc/data.hpp"
+#include "../inc/heart_model.hpp"
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+/// QT setup code starts here ///
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
+  QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreated,
-        &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
+/// QT setup code ends here ///
 
-    Heart *heart_model = Heart::getInstance();
-    QString color_opt_node[]={"lime","red","yellow"};
-    QString color_opt_path[]={"blue","lime","yellow","black","red"};
+  Heart *heart_model = Heart::getInstance();
+  QString color_opt_node[] = {"lime", "red", "yellow"};
+  QString color_opt_path[] = {"blue", "lime", "yellow", "black", "red"};
 
-    QVariantList nodeList;
-    for (int i = 0; i < Data.node_names.size(); ++i)
-        nodeList.emplace_back(QVariantMap{{"x", Data.node_positions[i].x},{"y", Data.node_positions[i].y},{"c", "yellow"}});
+  QVariantList nodeList;
+  for (int i = 0; i < Data.node_names.size(); ++i)
+    nodeList.emplace_back(QVariantMap{{"x", Data.node_positions[i].x},
+                                      {"y", Data.node_positions[i].y},
+                                      {"c", "yellow"}});
 
-    QVariantList pathList;
-    for (int i = 0; i < Data.path_names.size(); ++i) {
-        float degree = 180.0/3.14*std::atan2(Data.path_float_parameters[i][7], -1);
-        float ent_x = Data.node_positions[Data.path_int_parameters[i][1]-1].x;
-        float ent_y = Data.node_positions[Data.path_int_parameters[i][1]-1].y;
-        float out_x = Data.node_positions[Data.path_int_parameters[i][2]-1].x;
-        float out_y = Data.node_positions[Data.path_int_parameters[i][2]-1].y;
-        if(ent_x <= out_x) {ent_x = out_x; ent_y = out_y;}
-        pathList.emplace_back(QVariantMap{{"x", ent_x},{"y", ent_y},{"c", "blue"},
-                                          {"l", Data.path_float_parameters[i][6]},
-                                          {"d", degree}});
+  QVariantList pathList;
+  for (int i = 0; i < Data.path_names.size(); ++i) {
+    float degree =
+        180.0 / 3.14 * std::atan2(Data.path_float_parameters[i][7], -1);
+    float ent_x = Data.node_positions[Data.path_int_parameters[i][1] - 1].x;
+    float ent_y = Data.node_positions[Data.path_int_parameters[i][1] - 1].y;
+    float out_x = Data.node_positions[Data.path_int_parameters[i][2] - 1].x;
+    float out_y = Data.node_positions[Data.path_int_parameters[i][2] - 1].y;
+    if (ent_x <= out_x) {
+      ent_x = out_x;
+      ent_y = out_y;
     }
+    pathList.emplace_back(QVariantMap{{"x", ent_x},
+                                      {"y", ent_y},
+                                      {"c", "blue"},
+                                      {"l", Data.path_float_parameters[i][6]},
+                                      {"d", degree}});
+  }
 
+  QObject *rootObject = engine.rootObjects().first();
+  if (rootObject) {
+    QTimer *timer = new QTimer(rootObject);
+    int delay = 10;
+    int index = 0;
 
-    QObject *rootObject = engine.rootObjects().first();
-    if (rootObject) {
-        QTimer *timer = new QTimer(rootObject);
-        int delay = 10;
-        int index = 0;
+    QObject::connect(timer, &QTimer::timeout, rootObject, [&] {
+      if (index < 10'000'000)  // Data.node_positions.size())
+      {
+        rootObject->setProperty("timerLabel", index);
+        heart_model->heart_automaton();
+        for (int i = 0; i < heart_model->getNodeTable().node_table.size();
+             ++i) {
+          const QVariantMap &originalNode = nodeList.at(i).toMap();
+          QVariantMap modifiedElement = originalNode;
+          modifiedElement["c"] = color_opt_node[heart_model->getNodeTable()
+                                                    .node_table[i]
+                                                    .getParameters()
+                                                    .node_state_index -
+                                                1];
+          nodeList.replace(i, modifiedElement);
+        }
+        for (int i = 0; i < heart_model->getPathTable().path_table.size();
+             ++i) {
+          const QVariantMap &originalPath = pathList.at(i).toMap();
+          QVariantMap modifiedElement = originalPath;
+          int psi = heart_model->getPathTable()
+                        .path_table[i]
+                        .getParameters()
+                        .path_state_index -
+                    1;
+          // std::cout << psi << std::endl;
+          modifiedElement["c"] = color_opt_path[psi];
+          pathList.replace(i, modifiedElement);
+        }
+        rootObject->setProperty("pointData", QVariant::fromValue(nodeList));
+        rootObject->setProperty("pathData", QVariant::fromValue(pathList));
+        index++;
+      } else {
+        // Stop the timer when all updates are done
+        timer->stop();
+        std::cout << "Else" << std::endl;
+      }
+    });
+    // Start the timer
+    timer->start(delay);
+  }
 
-        QObject::connect(timer, &QTimer::timeout, rootObject, [&]{
-            if (index < 1'000'000)//Data.node_positions.size())
-            {
-                rootObject->setProperty("timerLabel", index);
-                heart_model->heart_automaton();
-                for (int i = 0; i < heart_model->getNodeTable().node_table.size(); ++i) {
-                    const QVariantMap& originalNode = nodeList.at(i).toMap();
-                    QVariantMap modifiedElement = originalNode;
-                    modifiedElement["c"] = color_opt_node[heart_model->getNodeTable().node_table[i].getParameters().node_state_index - 1];
-                    nodeList.replace(i, modifiedElement);
-                }
-                for (int i = 0; i < heart_model->getPathTable().path_table.size(); ++i) {
-                    const QVariantMap& originalPath = pathList.at(i).toMap();
-                    QVariantMap modifiedElement = originalPath;
-                    int psi = heart_model->getPathTable().path_table[i].getParameters().path_state_index - 1;
-                    // std::cout << psi << std::endl;
-                    modifiedElement["c"] = color_opt_path[psi];
-                    pathList.replace(i, modifiedElement);
-                }
-                rootObject->setProperty("pointData", QVariant::fromValue(nodeList));
-                rootObject->setProperty("pathData", QVariant::fromValue(pathList));
-                index++;
-            }
-            else {
-                // Stop the timer when all updates are done
-                timer->stop();
-                std::cout << "Else" << std::endl;
-            }
-        });
-
-        // Start the timer
-        timer->start(delay);
-
-    }
-
-    return app.exec();
+  return app.exec();
 }
