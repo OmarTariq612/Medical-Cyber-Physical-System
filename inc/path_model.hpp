0e72efaefec3ca40462c1db6655a63c62c1f9895
Abdo Eid <abdoeid.eg@ieee.org>
2024-04-18 03:02:38 +0200

    Formatted and cleaned path_model.*

--- inc/path_model.hpp
+++ inc/path_model.hpp
@@ -1,59 +1,82 @@
 #ifndef _PATH_MODEL_HPP
 #define _PATH_MODEL_HPP
 #include <string>
+#include <unordered_map>
 #include <vector>
-#include <unordered_map> 
+
 #include "GLOBAL_ENUMS.hpp"
 #include "data.hpp"
-class NodeTable;    // Forward declaration of NodeTable to avoid circular dependency
+class NodeTable;  // Forward declaration of NodeTable to avoid circular
+                  // dependency
 
-class Path{
-public:
-    Path(
-        const std::string &path_name,
-        const std::vector<int> &path_integer_parameters,
-        const std::vector<float> &path_float_parameters
-    );
-    // ~Path();
-    // void path_automaton(NodeTable& NT);
-    std::pair<bool, bool> path_automaton(NodeTable& NT);
+class Path {
+ public:
+  Path(const std::string &path_name,
+       const std::vector<int> &path_integer_parameters,
+       const std::vector<float> &path_float_parameters);
+  // ~Path();
 
-    // Getter methods
-    const path_parameters& getParameters() const { return _path_para; }
-    // Setter methods
-    void setParameters(const path_parameters& parameters) { _path_para = parameters; }
-    void setPathName(const std::string &path_name) { _path_para.path_name = path_name; }
-    void setPathStateIndex(const path_state_index_enum &path_state_index) { _path_para.path_state_index = path_state_index; }
-    void setEntryNodeIndex(const int &entry_node_index) { _path_para.entry_node_index = entry_node_index; }
-    void setExitNodeIndex(const int &exit_node_index) { _path_para.exit_node_index = exit_node_index; }
-    void setAmplitudeFactor(const int &amplitude_factor) { _path_para.amplitude_factor = amplitude_factor; }
-    void setForwardSpeed(const float &forward_speed) { _path_para.forward_speed = forward_speed; }
-    void setBackwardSpeed(const float &backward_speed) { _path_para.backward_speed = backward_speed; }
-    void setForwardTimerCurrent(const float &forward_timer_current) { _path_para.forward_timer_current = forward_timer_current; }
-    void setForwardTimerDefault(const float &forward_timer_default) { _path_para.forward_timer_default = forward_timer_default; }
-    void setBackwardTimerCurrent(const float &backward_timer_current) { _path_para.backward_timer_current = backward_timer_current; }
-    void setBackwardTimerDefault(const float &backward_timer_default) { _path_para.backward_timer_default = backward_timer_default; }
-    void setPathLength(const float &path_length) { _path_para.path_length = path_length; }
-    void setPathSlope(const float &path_slope) { _path_para.path_slope = path_slope; }
+  std::pair<bool, bool> path_automaton(NodeTable &NT);
 
+  // Getter methods
+  const path_parameters &getParameters() const { return _path_para; }
+  // Setter methods
+  void setParameters(const path_parameters &parameters) {
+    _path_para = parameters;
+  }
+  void setPathName(const std::string &path_name) {
+    _path_para.path_name = path_name;
+  }
+  void setPathStateIndex(const path_state_index_enum &path_state_index) {
+    _path_para.path_state_index = path_state_index;
+  }
+  void setEntryNodeIndex(const int &entry_node_index) {
+    _path_para.entry_node_index = entry_node_index;
+  }
+  void setExitNodeIndex(const int &exit_node_index) {
+    _path_para.exit_node_index = exit_node_index;
+  }
+  void setAmplitudeFactor(const int &amplitude_factor) {
+    _path_para.amplitude_factor = amplitude_factor;
+  }
+  void setForwardSpeed(const float &forward_speed) {
+    _path_para.forward_speed = forward_speed;
+  }
+  void setBackwardSpeed(const float &backward_speed) {
+    _path_para.backward_speed = backward_speed;
+  }
+  void setForwardTimerCurrent(const float &forward_timer_current) {
+    _path_para.forward_timer_current = forward_timer_current;
+  }
+  void setForwardTimerDefault(const float &forward_timer_default) {
+    _path_para.forward_timer_default = forward_timer_default;
+  }
+  void setBackwardTimerCurrent(const float &backward_timer_current) {
+    _path_para.backward_timer_current = backward_timer_current;
+  }
+  void setBackwardTimerDefault(const float &backward_timer_default) {
+    _path_para.backward_timer_default = backward_timer_default;
+  }
+  void setPathLength(const float &path_length) {
+    _path_para.path_length = path_length;
+  }
+  void setPathSlope(const float &path_slope) {
+    _path_para.path_slope = path_slope;
+  }
 
-private:
-    path_parameters _path_para;
+ private:
+  path_parameters _path_para;
 };
-
 
 // root singleton class for the path table
-class PathTable{
-public:
-    PathTable(
-        const std::vector<std::string> &path_names,
-        const std::vector<std::vector<int>> &path_integer_parameters,
-        const std::vector<std::vector<float>> &path_float_parameters);
-    // ~PathTable();
-    std::vector<std::vector<path_terminal_pair>> path_terminal_pairs_per_point_list; // TODO: make this private
-    std::vector<Path> path_table;       // TODO prevent direct access to this variable
-
-    // pathe_terminal_pairs_per_point getter
-    // const std::vector<std::vector<path_terminal_pair>>& getPathTerminalPairsPerPoint() const { return path_terminal_pairs_per_point_list; }
+class PathTable {
+ public:
+  PathTable(const std::vector<std::string> &path_names,
+            const std::vector<std::vector<int>> &path_integer_parameters,
+            const std::vector<std::vector<float>> &path_float_parameters);
+  // ~PathTable();
+  std::vector<std::vector<path_terminal_pair>>
+      path_terminal_pairs_per_point_list;  // TODO(abdoei): make this private
+  std::vector<Path> path_table;  // TODO(abdoei): prevent direct access to this variable
 };
-#endif // _PATH_MODEL_HPP
+#endif  // _PATH_MODEL_HPP
--- src/path_model.cpp
+++ src/path_model.cpp
@@ -4,186 +4,162 @@
 #include "../inc/node_model.hpp"
 
 // Constructor
-Path::Path(
-    const std::string &path_name_,
-    const std::vector<int> &path_integer_parameters,
-    const std::vector<float> &path_float_parameters)
-    : 
-    _path_para{
-        path_name_,
-        static_cast<path_state_index_enum>(path_integer_parameters[0]), // (1) Idle, (2) Antegrade conduction, (3) Retrograde, (4) Conflict, and (5) Double
-        path_integer_parameters[1] - 1,                                 // entry_node_index = 0 (originally 1)
-        path_integer_parameters[2] - 1,                                 // exit_node_index = 1 (originally 2)
-        path_integer_parameters[3],                                     // amplitude_factor
-        path_float_parameters[0],                                       // forward_speed
-        path_float_parameters[1],                                       // backward_speed
-        path_float_parameters[2],                                       // forward_timer_current
-        path_float_parameters[3],                                       // forward_timer_default
-        path_float_parameters[4],                                       // backward_timer_current
-        path_float_parameters[5],                                       // backward_timer_default
-        path_float_parameters[6],                                       // path_length
-        path_float_parameters[7]                                        // path_slope
-    }
-{
-}
+Path::Path(const std::string &path_name_,
+           const std::vector<int> &path_integer_parameters,
+           const std::vector<float> &path_float_parameters)
+    : _path_para{
+          path_name_,
+          static_cast<path_state_index_enum>(
+              path_integer_parameters[0]),  // (1) Idle, (2) Antegrade
+                                            // conduction, (3) Retrograde, (4)
+                                            // Conflict, and (5) Double
+          path_integer_parameters[1] - 1,   // entry_node_index=0 (originally 1)
+          path_integer_parameters[2] - 1,   // exit_node_index=1 (originally 2)
+          path_integer_parameters[3],       // amplitude_factor
+          path_float_parameters[0],         // forward_speed
+          path_float_parameters[1],         // backward_speed
+          path_float_parameters[2],         // forward_timer_current
+          path_float_parameters[3],         // forward_timer_default
+          path_float_parameters[4],         // backward_timer_current
+          path_float_parameters[5],         // backward_timer_default
+          path_float_parameters[6],         // path_length
+          path_float_parameters[7]          // path_slope
+      } {}
 
 PathTable::PathTable(
     const std::vector<std::string> &path_names_,
     const std::vector<std::vector<int>> &path_integer_parameters_,
-    const std::vector<std::vector<float>> &path_float_parameters_)
-{
-    for (int i = 0; i < path_names_.size(); ++i)
-    {
-        Path path1(path_names_[i], path_integer_parameters_[i], path_float_parameters_[i]);
-        path_table.push_back(path1);
-    }
+    const std::vector<std::vector<float>> &path_float_parameters_) {
+  for (int i = 0; i < path_names_.size(); ++i) {
+    Path path1(path_names_[i], path_integer_parameters_[i],
+               path_float_parameters_[i]);
+    path_table.push_back(path1);
+  }
 
-    std::unordered_map<int, std::vector<path_terminal_pair>> node_idx_paths_terminals_umap;
-    for (int path_idx = 0; path_idx < path_table.size(); ++path_idx)
-    {
-        int entry_idx = path_table[path_idx].getParameters().entry_node_index;
-        int exit_idx = path_table[path_idx].getParameters().exit_node_index;
-        node_idx_paths_terminals_umap[entry_idx].push_back(path_terminal_pair{path_idx, __entry});
-        node_idx_paths_terminals_umap[exit_idx].push_back(path_terminal_pair{path_idx, __exit});
-    }
-    int node_count = Data.node_names.size();
-    // made this instead of using the node_table.size() because the node_table is not initialized yet and it will go into a cyclic dependency problem
-    for (int node_idx = 0; node_idx < node_count; ++node_idx)
-    {
-        path_terminal_pairs_per_point_list.push_back(node_idx_paths_terminals_umap[node_idx]);
-    }
+  std::unordered_map<int, std::vector<path_terminal_pair>>
+      node_idx_paths_terminals_umap;
+  for (int path_idx = 0; path_idx < path_table.size(); ++path_idx) {
+    int entry_idx = path_table[path_idx].getParameters().entry_node_index;
+    int exit_idx = path_table[path_idx].getParameters().exit_node_index;
+    node_idx_paths_terminals_umap[entry_idx].push_back(
+        path_terminal_pair{path_idx, __entry});
+    node_idx_paths_terminals_umap[exit_idx].push_back(
+        path_terminal_pair{path_idx, __exit});
+  }
+  // used node_count instead of using the node_table.size() because the
+  // node_table is not initialized yet and it will go into a cyclic dependency
+  // problem
+  int node_count = Data.node_names.size();
+  for (int node_idx = 0; node_idx < node_count; ++node_idx) {
+    path_terminal_pairs_per_point_list.push_back(
+        node_idx_paths_terminals_umap[node_idx]);
+  }
 }
 
-// Path automaton
-std::pair<bool, bool> Path::path_automaton(NodeTable& NT){
-    /*
-    in the _path_para it changes:
-        1. path_state_index         [2]
-        2. forward_timer_current    [8]
-        3. backward_timer_current   [10]
-    in the NT it changes:
-        1. activation               [9]
-    */
-
-
-    bool temp_node1_activation = false;
-    bool temp_node2_activation = false;
-    const int entry_node_index = _path_para.entry_node_index;
-    const int exit_node_index = _path_para.exit_node_index;
-    Node &entry_node = NT.node_table[entry_node_index];
-    Node &exit_node = NT.node_table[exit_node_index];
-    switch (_path_para.path_state_index)
-    {
-    case Idle: // Idle
-        // if activation coming from entry node
-        if (entry_node.getParameters().activation == true)
-            // Antegrade conduction
-            _path_para.path_state_index = Antegrade_conduction;
-        // if activation coming from exit node
-        else if (exit_node.getParameters().activation == true)
-            // Retrograde conduction
-            _path_para.path_state_index = Retrograde;
-        break;
-    case Antegrade_conduction: // Antegrade conduction
-        // if activation coming from exit node
-        if (exit_node.getParameters().activation == true)
-        { // double
-            _path_para.path_state_index = Double;
-            // // reset timer
-            // _path_para.forward_timer_current = _path_para.forward_timer_default;
-        }
-        else
-        {
-            // if timer running out
-            if (_path_para.forward_timer_current == 0)
-            {
-                // reset timer
-                _path_para.forward_timer_current = _path_para.forward_timer_default;
-                // activate exit node
-                temp_node2_activation = true;
-                // go to conflict state
-                _path_para.path_state_index = Conflict;
-            }
-            else // timer
-                _path_para.forward_timer_current--;
-        }
-        break;
-    case Retrograde: // Retro
-        // if activation coming from entry node
-        if (entry_node.getParameters().activation == true)
-        { // conflict
-            // go to Double state
-            _path_para.path_state_index = Double;
-            // // reset timer
-            // _path_para.backward_timer_current = _path_para.backward_timer_default;
-        }
-        else
-        {
-            // if timer runs out
-            if (_path_para.backward_timer_current == 0)
-            {
-                // reset timer
-                _path_para.backward_timer_current = _path_para.backward_timer_default;
-                // activate the entry node
-                temp_node1_activation = true;
-                // change state to conflict
-                _path_para.path_state_index = Conflict;
-            }
-            else // timer
-                _path_para.backward_timer_current--;
-        }
-        break;
-    case Conflict: // Conflict
-        // go to Idle state
-        _path_para.path_state_index = Idle;
-        break;
-    case Double: // double
-        if (_path_para.backward_timer_current == 0)
-        {
-            // reset timer
-            _path_para.backward_timer_current = _path_para.backward_timer_default;
-            // activate the entry node
-            temp_node1_activation = true;
-            // change state to conflict
-            // path_para{2}=2; TODO: check this
-            _path_para.path_state_index = Antegrade_conduction;
-            // _path_para.path_state_index = Conflict;
-            // Update the path nodes activation before returning
-            // entry_node.setActivation(temp_node1_activation);
-            // exit_node.setActivation(temp_node2_activation);
-            return {temp_node1_activation, temp_node2_activation};
-            // return;
-        }
-        if (_path_para.forward_timer_current == 0)
-        {
-            // reset timer
-            _path_para.forward_timer_current = _path_para.forward_timer_default;
-            // activate exit node
-            temp_node2_activation = true;
-            // go to conflict state
-            // path_para{2}=3; TODO: check this too
-            _path_para.path_state_index = Retrograde;
-            // _path_para.path_state_index = Conflict;
-            // Update the path nodes activation before returning
-            // entry_node.setActivation(temp_node1_activation);
-            // exit_node.setActivation(temp_node2_activation);
-            return {temp_node1_activation, temp_node2_activation};
-            // return;
-        }
-        if (std::abs(1 - _path_para.forward_timer_current / _path_para.forward_timer_default - _path_para.backward_timer_current / _path_para.backward_timer_default) < (0.9 / std::min(_path_para.forward_timer_default, _path_para.backward_timer_default)))
-        {
-            _path_para.backward_timer_current = _path_para.backward_timer_default;
-            _path_para.forward_timer_current = _path_para.forward_timer_default;
-            _path_para.path_state_index = Conflict;
-        }
-        else
-        {
-            _path_para.forward_timer_current--;
-            _path_para.backward_timer_current--;
-        }
-        break;
-    }
-    return {temp_node1_activation, temp_node2_activation};
+/*
+ * This function updates the status of a single path
+ */
+std::pair<bool, bool> Path::path_automaton(NodeTable &NT) {
+  /*
+  in the _path_para it changes:
+      1. path_state_index         [2]
+      2. forward_timer_current    [8]
+      3. backward_timer_current   [10]
+  in the NT it changes:
+      1. activation               [9]
+  */
+  bool temp_node1_activation = false;
+  bool temp_node2_activation = false;
+  const int entry_node_index = _path_para.entry_node_index;
+  const int exit_node_index = _path_para.exit_node_index;
+  Node &entry_node = NT.node_table[entry_node_index];
+  Node &exit_node = NT.node_table[exit_node_index];
+  switch (_path_para.path_state_index) {
+    case Idle:  // Idle
+      // if activation coming from entry node
+      if (entry_node.getParameters().activation == true)
+        // Antegrade conduction
+        _path_para.path_state_index = Antegrade_conduction;
+      // if activation coming from exit node
+      else if (exit_node.getParameters().activation == true)
+        // Retrograde conduction
+        _path_para.path_state_index = Retrograde;
+      break;
+    case Antegrade_conduction:  // Antegrade conduction
+      // if activation coming from exit node
+      if (exit_node.getParameters().activation == true) {  // double
+        _path_para.path_state_index = Double;
+      } else {
+        // if timer running out
+        if (_path_para.forward_timer_current == 0) {
+          // reset timer
+          _path_para.forward_timer_current = _path_para.forward_timer_default;
+          // activate exit node
+          temp_node2_activation = true;
+          // go to conflict state
+          _path_para.path_state_index = Conflict;
+        } else  // timer
+          _path_para.forward_timer_current--;
+      }
+      break;
+    case Retrograde:  // Retro
+      // if activation coming from entry node
+      if (entry_node.getParameters().activation == true) {  // conflict
+        // go to Double state
+        _path_para.path_state_index = Double;
+      } else {
+        // if timer runs out
+        if (_path_para.backward_timer_current == 0) {
+          // reset timer
+          _path_para.backward_timer_current = _path_para.backward_timer_default;
+          // activate the entry node
+          temp_node1_activation = true;
+          // change state to conflict
+          _path_para.path_state_index = Conflict;
+        } else  // timer
+          _path_para.backward_timer_current--;
+      }
+      break;
+    case Conflict:  // Conflict
+      // go to Idle state
+      _path_para.path_state_index = Idle;
+      break;
+    case Double:  // double
+      if (_path_para.backward_timer_current == 0) {
+        // reset timer
+        _path_para.backward_timer_current = _path_para.backward_timer_default;
+        // activate the entry node
+        temp_node1_activation = true;
+        // change state to conflict
+        _path_para.path_state_index = Conflict;
+        return {temp_node1_activation, temp_node2_activation};
+      }
+      if (_path_para.forward_timer_current == 0) {
+        // reset timer
+        _path_para.forward_timer_current = _path_para.forward_timer_default;
+        // activate exit node
+        temp_node2_activation = true;
+        // go to conflict state
+        _path_para.path_state_index = Conflict;
+        return {temp_node1_activation, temp_node2_activation};
+      }
+      if (std::abs(1 -
+                   _path_para.forward_timer_current /
+                       _path_para.forward_timer_default -
+                   _path_para.backward_timer_current /
+                       _path_para.backward_timer_default) <
+          (0.9 / std::min(_path_para.forward_timer_default,
+                          _path_para.backward_timer_default))) {
+        _path_para.backward_timer_current = _path_para.backward_timer_default;
+        _path_para.forward_timer_current = _path_para.forward_timer_default;
+        _path_para.path_state_index = Conflict;
+      } else {
+        _path_para.forward_timer_current--;
+        _path_para.backward_timer_current--;
+      }
+      break;
+  }
+  return {temp_node1_activation, temp_node2_activation};
 }
 
-#endif // _PATH_MODEL_CPP
+#endif  // _PATH_MODEL_CPP
